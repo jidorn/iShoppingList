e465b3c7cbfd832650a43bdcd83c27368b8c1a02
etudiant <etudiant@iMac-de-etudiant-99.local>
2015-02-25 15:08:14 +0100

    debut project

---
+++ iShoppingList/user.h
@@ -0,0 +1,13 @@
+//
+//  user.h
+//  iShoppingList
+//
+//  Created by etudiant on 25/02/2015.
+//  Copyright (c) 2015 JD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface user : NSObject
+
+@end
---
+++ iShoppingList/user.m
@@ -0,0 +1,13 @@
+//
+//  user.m
+//  iShoppingList
+//
+//  Created by etudiant on 25/02/2015.
+//  Copyright (c) 2015 JD. All rights reserved.
+//
+
+#import "user.h"
+
+@implementation user
+
+@end
